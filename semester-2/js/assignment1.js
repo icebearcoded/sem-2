8aa4bc99a94c95bf32f8b3fb22e06e2cb2a99b09
yumnahussain6 <yumna.hosain@gmail.com>
2025-02-16 04:53:40 -0800

    js

--- semester-2/js/assignment1.js
+++ semester-2/js/assignment1.js
@@ -12,15 +12,52 @@ if (math && cs && physics && urdu && eng) {
 
     if (math <= 100 && cs <= 100 && physics <= 100 && urdu <= 100 && eng <= 100) {
         if (percentage >= 80 && percentage <= 100) {
-            document.getElementById('heading').innerHTML += `<h1>Your Grade is A1<h1>`
+            document.getElementById('heading').innerHTML+=`<h1>Your percentage is: ${percentage}%<h1>`;
+            document.getElementById('heading').innerHTML += `<h1>Your Grade is A1<h1>`   
+            document.getElementById('heading2').innerHTML+=`<h2>Your Mathematics marks are: ${math}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Computer Science marks are: ${cs}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Physics marks are: ${physics}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Urdu marks are: ${urdu}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your English marks are: ${eng}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Total marks are: ${obtained}/500<h2>`;
+            
+
         } else if (percentage >= 70 && percentage <= 79.9) {
-            document.getElementById('heading').innerHTML += `<h1>Your Grade is A<h1>`
+            document.getElementById('heading').innerHTML+=`<h1>Your percentage is: ${percentage}%<h1>`;
+            document.getElementById('heading').innerHTML += `<h1>Your Grade is A<h1>`        
+            document.getElementById('heading2').innerHTML+=`<h2>Your Mathematics marks are: ${math}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Computer Science marks are: ${cs}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Physics marks are: ${physics}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Urdu marks are: ${urdu}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your English marks are: ${eng}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Total marks are: ${obtained}/500<h2>`;
         } else if (percentage >= 60 && percentage <= 69.9) {
+            document.getElementById('heading').innerHTML+=`<h1>Your percentage is: ${percentage}%<h1>`;
             document.getElementById('heading').innerHTML += `<h1>Your Grade is B<h1>`
+            document.getElementById('heading2').innerHTML+=`<h2>Your Mathematics marks are: ${math}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Computer Science marks are: ${cs}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Physics marks are: ${physics}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Urdu marks are: ${urdu}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your English marks are: ${eng}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Total marks are: ${obtained}/500<h2>`;
         } else if (percentage >= 50 && percentage <= 59.9) {
+            document.getElementById('heading').innerHTML+=`<h1>Your percentage is: ${percentage}%<h1>`;
             document.getElementById('heading').innerHTML += `<h1>Your Grade is C<h1>`
+            document.getElementById('heading2').innerHTML+=`<h2>Your Mathematics marks are: ${math}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Computer Science marks are: ${cs}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Physics marks are: ${physics}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Urdu marks are: ${urdu}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your English marks are: ${eng}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Total marks are: ${obtained}/500<h2>`;
         } else if (percentage >= 40 && percentage <= 49.9) {
+            document.getElementById('heading').innerHTML+=`<h1>Your percentage is: ${percentage}%<h1>`;
             document.getElementById('heading').innerHTML += `<h1>Your Grade is D<h1>`
+            document.getElementById('heading2').innerHTML+=`<h2>Your Mathematics marks are: ${math}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Computer Science marks are: ${cs}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Physics marks are: ${physics}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Urdu marks are: ${urdu}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your English marks are: ${eng}<h2>`;
+            document.getElementById('heading2').innerHTML+=`<h2>Your Total marks are: ${obtained}/500<h2>`;
         } else {
             document.getElementById('heading').innerHTML += `<h1>You have failed.<h1>`
         }
